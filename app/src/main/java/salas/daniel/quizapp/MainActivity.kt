a56c213f0faa9517dc647f6907a53d09e647e690
DanielSalas11 <daniel_salas11@outlook.com>
2023-10-24 18:24:49 -0700

    UI update, added traveling through activities

--- app/src/main/java/salas/daniel/quizapp/MainActivity.kt
+++ app/src/main/java/salas/daniel/quizapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package salas.daniel.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart : Button = findViewById(R.id.btn_Start)
+        val etName : EditText = findViewById(R.id.etName)
+
+        btnStart.setOnClickListener {
+            if(etName.text.isEmpty()){
+                Toast.makeText(this, "Please enter your name.", Toast.LENGTH_SHORT).show()
+            }else{
+                val intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
     }
 }
---
+++ app/src/main/java/salas/daniel/quizapp/QuizQuestionsActivity.kt
@@ -0,0 +1,13 @@
+package salas.daniel.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizQuestionsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz_questions)
+    }
+
+
+}
